3f25c2c43d6df6224d40942917d471566fcc2c1e
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-13 21:09:55 -0700

    Added content to AboutMe section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Header from "./components/navbar";
-import About from "./components/About";
-import Projects from "./components/Projects";
+import Header from "./components/header";
+import About from "./pages/aboutMe";
+import Projects from "./pages/projects";
 import Resume from "./components/downloadResume";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 
 function App() {
   return (
--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,5 @@ import Card from 'react-bootstrap/Card';
       This is a wider card with supporting text below as a natural lead-in to
       additional content. This content is a little bit longer.
     </Card.Text>
-    <Card.Text>Last updated 3 mins ago</Card.Text>
   </Card.ImgOverlay>
 </Card>
