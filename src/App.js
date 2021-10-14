d97ed384d43acf79d7a73207f1a0f641171944c9
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-13 20:41:23 -0700

    Used images from previous portfolio, included navbar, header, and footer jsx boilerplate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
+import React from "react";
+import Header from "./components/navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Resume from "./components/downloadResume";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <About />
+      <Projects />
+      <Resume />
+      <Footer />
     </div>
   );
 }
