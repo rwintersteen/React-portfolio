8ce0f964872f32301f6d2ff0791f2a64c8685ba4
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-30 22:07:07 -0700

    Added tablist for about me, projects, contact, resume

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,38 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav';
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "../Tabs.css";
+import About from "/aboutMe";
+import ProjectCards from "/projects";
+import Contact from "/contact";
+import Resume from "/downloadResume";
 
-function NavBar() {
-  
+function NavTabs {
   return (
-    <Navbar bg="light" variant="light">
-      <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-      </Container>
-    </Navbar>
-  );
-}
+    <div className="container">
+      <Tabs>
+      <TabList>
+          <Tab >About Me</Tab>
+          <Tab >Projects</Tab>
+          <Tab >Contact Me</Tab>
+          <Tab >Download Resume</Tab>
+      </TabList>
 
-export default NavBar;
+      <TabPanel>
+        <About />
+      </TabPanel>
+
+      <TabPanel>
+        <ProjectCards />
+      </TabPanel>
+
+      <TabPanel>
+        <Contact />
+      </TabPanel>
+
+      <TabPanel>
+        <Resume />
+      </TabPanel>
+
+      </Tabs>
+  )
+}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,3 +1,58 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import 
+import "../Tabs.css";
+
+
+function ProjectCards() {
+    return (
+        <div>
+            <Card className='card-border'>
+                <Card.Body>
+                    <Card.Title> <h2>Note Taker</h2> </Card.Title>
+                        <Card.Text>
+                            <h6>A Note Taking application that uses Express.js and is also depolyed to Heroku.</h6>
+                        </Card.Text>
+                    </Card.Body>
+            </Card>
+
+            <Card className='card-border'>
+                <Card.Body>
+                    <Card.Title> <h2>Dish Up</h2> </Card.Title>
+                        <Card.Text>
+                            <h6>A group project that allows users to search through thousands of recipes with the help of the Spoonacular API.</h6>
+                        </Card.Text>
+                    </Card.Body>
+            </Card>
+
+            <Card className='card-border'>
+                <Card.Body>
+                    <Card.Title> <h2>Team Profile Generator</h2> </Card.Title>
+                        <Card.Text>
+                            <h6>Using Inquirer, a team profile generator that builds cards based on NodeJS command line input from a user.</h6>
+                        </Card.Text>
+                    </Card.Body>
+            </Card>
+
+            <Card className='card-border'>
+                <Card.Body>
+                    <Card.Title> <h2>NewsFlash</h2> </Card.Title>
+                        <Card.Text>
+                            <h6>A news aggreagate website which focused on building the backend with a group of fellow students.</h6>
+                        </Card.Text>
+                    </Card.Body>
+            </Card>
+
+            <Card className='card-border'>
+                <Card.Body>
+                    <Card.Title> <h2>Piano on the Go</h2> </Card.Title>
+                        <Card.Text>
+                            <h6>The final group project </h6>
+                        </Card.Text>
+                    </Card.Body>
+            </Card>
+
+            
+        </div>
+    )
+}
+export default ProjectCards;
