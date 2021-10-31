fbf38eeb2123b0797b59163a84c950feab382430
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-30 21:26:43 -0700

    Added About Me section, used text from updated portfolio

---
+++ src/components/aboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import '../App.css';
+
+function AboutMe() {
+    return ( 
+        <div>
+            <h3 className="h3-text" id="title">About Me</h3>
+                <h2 className="h2-text">Over the last decade, my career focus has been solely in dentistry and making my way through orthodontic assisting. I have filled just about every role in the dental clinic with my current credentials. However, I needed a fresh start and web development was my siren's call. I enrolled in the University of Oregon's Coding Bootcamp in April 2021 and expect to graduate with a certificate of completion in October 2021.
+                </h2>
+        </div>
+)}
+
+export default AboutMe;
